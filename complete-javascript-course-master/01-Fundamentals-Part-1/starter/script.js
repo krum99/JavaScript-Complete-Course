6d60bdda6e96470e797d4aadbd13898d6b4e8b6e
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-29 16:55:39 +0200

    strict vs loose equal 2

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -9,15 +9,28 @@
 // console.log(continent);
 // console.log(population);
 
-let number = 18;
-if (number == '18'){
-  console.log('loose equal')
-}
+// let number = 18;
+// if (number == '18'){
+//   console.log('loose equal')
+// }
 
-if (number === 18){
-  console.log('strict equal')
-}
+// if (number === 18){
+//   console.log('strict equal')
+// }
 
-if (!(number === '18')){
-  console.log(`number 18 not strictly equal to '${18}'`)
+// if (!(number === '18')){
+//   console.log(`number 18 not strictly equal to '${18}'`)
+// }
+
+// const favourite = Number(prompt("Favourite number ?"))
+
+// console.log(favourite)
+// console.log(typeof favourite)
+
+// if (favourite === 23){
+//    console.log("23 is amazing number")
+// } else if(favourite === 7){
+//   console.log("7 is also cool number!")
+// } else{
+//   console.log("Number is not 27 or 7")
 }
