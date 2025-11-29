3db7b28f54339ab38308a51efa74ef194122341c
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-29 16:50:47 +0200

    strict vs loose equal

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,23 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JS is Fun!');
+// let js = 'amazing';
+// if (js === 'amazing') alert('JS is Fun!');
 
-let country = 'Bulgaria';
-let continent = 'Europe';
-let population = 10;
+// let country = 'Bulgaria';
+// let continent = 'Europe';
+// let population = 10;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
+let number = 18;
+if (number == '18'){
+  console.log('loose equal')
+}
 
+if (number === 18){
+  console.log('strict equal')
+}
+
+if (!(number === '18')){
+  console.log(`number 18 not strictly equal to '${18}'`)
+}
