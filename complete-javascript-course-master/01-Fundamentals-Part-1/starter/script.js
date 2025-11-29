bf801e83cbfa8a8604b25011dd369adf2f8ff2ab
krum99 <79230210+krum99@users.noreply.github.com>
2025-11-29 16:46:07 +0200

    brief intro

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JS is Fun!');
+
+let country = 'Bulgaria';
+let continent = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
