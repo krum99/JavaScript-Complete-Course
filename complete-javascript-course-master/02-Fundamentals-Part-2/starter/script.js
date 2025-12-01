a2735ee7a8ff171c333c60bb55af85a27f0d66b7
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-01 17:33:27 +0200

    arrow functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -10,4 +10,16 @@ const calcAge2 = function calcAge2(birth) {
 
 const age2 = calcAge2(1999);
 
-console.log(age1, age2);
+const calcAge3 = (birth) => 2026 - birth;
+
+const age3 = calcAge3(1999);
+
+console.log(age1, age2, age3);
+
+const ageUntilRetirement = (birthYeah, firstName) => {
+  const age = 2026 - birthYeah;
+  const retirement = 65 - age;
+
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(ageUntilRetirement(1999, 'Krum'));
