44c0fc213d1e1652a12dc52a2a8de1fe2b8de2b0
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-01 19:31:20 +0200

    array introduction

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,25 +1,30 @@
-function calcAge1(birth) {
-  return 2026 - birth;
-}
+// function calcAge1(birth) {
+//   return 2026 - birth;
+// }
 
-const age1 = calcAge1(1999);
+// const age1 = calcAge1(1999);
 
-const calcAge2 = function calcAge2(birth) {
-  return 2026 - birth;
-};
+// const calcAge2 = function calcAge2(birth) {
+//   return 2026 - birth;
+// };
 
-const age2 = calcAge2(1999);
+// const age2 = calcAge2(1999);
 
-const calcAge3 = (birth) => 2026 - birth;
+// const calcAge3 = (birth) => 2026 - birth;
 
-const age3 = calcAge3(1999);
+// const age3 = calcAge3(1999);
 
-console.log(age1, age2, age3);
+// console.log(age1, age2, age3);
 
-const ageUntilRetirement = (birthYeah, firstName) => {
-  const age = 2026 - birthYeah;
-  const retirement = 65 - age;
+// const ageUntilRetirement = (birthYeah, firstName) => {
+//   const age = 2026 - birthYeah;
+//   const retirement = 65 - age;
 
-  return `${firstName} retires in ${retirement} years`;
-};
-console.log(ageUntilRetirement(1999, 'Krum'));
+//   return `${firstName} retires in ${retirement} years`;
+// };
+// console.log(ageUntilRetirement(1999, 'Krum'));
+
+const friends = [1, '23', 'stringsad'];
+
+console.log(friends);
+console.log(friends.length);
